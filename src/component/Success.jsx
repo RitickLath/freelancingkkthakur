f3314e362d52bdb0b58e9722fbd8667119cde92d
RitickLath <ritick943lath@gmail.com>
2024-11-06 20:50:12 +0530

    Success Section

--- src/component/Success.jsx
+++ src/component/Success.jsx
@@ -5,7 +5,7 @@ const Success = () => {
     <div className="md:flex justify-between py-16 text-white my-16 px-6 md:px-20 lg:px-32">
       {/* Header Section */}
       <div className="mb-12">
-        <h1 className="pt-6 text-3xl md:text-5xl font-bold">
+        <h1 className="pt-6 text-4xl md:text-5xl font-bold text-[#C68C33]">
           Trusted Results, <br /> Measurable Impact
         </h1>
       </div>
@@ -15,7 +15,7 @@ const Success = () => {
         {/* Uptime Commitment */}
         <div className="pb-6 md:pt-6 border-b-[1px]">
           <h1 className="text-3xl md:text-5xl font-semibold">99.9%</h1>
-          <h2 className="text-lg mt-2">
+          <h2 className="text-[#A7A3AC] text-lg mt-2">
             Uptime Commitment for reliable service.
           </h2>
         </div>
@@ -23,7 +23,7 @@ const Success = () => {
         {/* Services Covered */}
         <div className="py-6 border-b-[1px]">
           <h1 className="text-3xl md:text-5xl font-semibold">150+</h1>
-          <h2 className="text-lg mt-2">
+          <h2 className="text-[#A7A3AC] text-lg mt-2">
             Services across IT, software, and design.
           </h2>
         </div>
@@ -31,13 +31,13 @@ const Success = () => {
         {/* Project Turnaround */}
         <div className="py-6 border-b-[1px]">
           <h1 className="text-3xl md:text-5xl font-semibold">Fast</h1>
-          <h2 className="text-lg mt-2">Fast project deployment for growth.</h2>
+          <h2 className="text-[#A7A3AC] text-lg mt-2">Fast project deployment for growth.</h2>
         </div>
 
         {/* End-to-End Service Commitment */}
         <div className="py-6 border-b-[1px]">
           <h1 className="text-3xl md:text-5xl font-semibold">End-to-End</h1>
-          <h2 className="text-lg mt-2">Full service from start to finish.</h2>
+          <h2 className="text-[#A7A3AC] text-lg mt-2">Full service from start to finish.</h2>
         </div>
       </div>
     </div>
--- src/component/Why.jsx
+++ src/component/Why.jsx
@@ -5,7 +5,7 @@ import { whyChooseUs } from "../constants/WhyChoose";
 const Why = () => {
   return (
     <div className="z-10 flex flex-col items-center w-full mt-8">
-      <h1 className="text-4xl font-bold pb-8 text-center">
+      <h1 className="text-4xl md:text-4xl font-bold pb-8 text-center text-[#C68C33]">
         Why One Stop Solution?
       </h1>
       <div className="grid md:grid-cols-2 gap-10">
